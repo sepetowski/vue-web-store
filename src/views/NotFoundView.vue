dbe6af280f9b7fc35c79d6749cf714c0763ed202
Bushmeen <bushmeen@gmail.com>
2024-04-09 13:40:15 +0200

    styled 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import WrapperElement from '@/components/ui/WrapperElement.vue'
+</script>
 <template>
-  <main>404</main>
+  <main>
+    <WrapperElement>
+      <div class="flex justify-center items-center flex-col gap-4 mt-20">
+        <h1 class="text-8xl">404</h1>
+        <p class="text-3xl text-mutated-200">Page not found</p>
+      </div>
+    </WrapperElement>
+  </main>
 </template>
